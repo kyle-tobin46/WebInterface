0b21272f7ffdcddf234a5b7be51cf728e569e356
Kyle Tobin <kyle_tobin@outlook.com>
2024-10-21 00:03:01 -0700

    basic functionality

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all checkboxes
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-    // Add an event listener to each checkbox
     checkboxes.forEach(function(checkbox) {
         checkbox.addEventListener('change', function() {
             console.log(`${this.id} is ${this.checked ? 'enabled' : 'disabled'}`);
